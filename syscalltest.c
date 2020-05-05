41e9b2ea51282e2957beaa7aeb52f09f179ec2ee
FrodoProductions <freddy@aerofloyd.de>
2020-05-05 16:24:42 +0200

    Läuft

--- syscalltest.c
+++ syscalltest.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -12,12 +11,13 @@ int main(int argc, char const *argv[]) {
   // Diese Art der Verwendung von access() hab ich in diesem StackOverflow-Thread gefunden:
   // https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
   if (access(*++argv, F_OK) == 0) {
-
-    int in = open(*argv, O_RDONLY);
-    int inputsize = read(in, &puffer, 1024);
-    write(1, puffer, inputsize);
-    close(in);
-
+    for (size_t i = 0; i < sizeof(*argv); i++) {
+      int in = open(*argv, O_RDONLY);
+      int inputsize = read(in, &puffer, 1024);
+      write(1, puffer, inputsize);
+      close(in);
+      argv++;
+    }
   } else {
     write(1, *argv, sizeof(*argv));
     write(1, "\n", 1);
