b3c65416d4e6bffff1843209cc72d96cfa0240da
FrodoProductions <freddy@aerofloyd.de>
2020-05-05 16:15:41 +0200

    Der vorige commit, nur jetzt richtig

--- syscalltest.c
+++ syscalltest.c
@@ -9,12 +9,19 @@ int main(int argc, char const *argv[]) {
 
   char puffer[100];
 
-  int inch = open(*++argv, O_RDONLY);
+  // Diese Art der Verwendung von access() hab ich in diesem StackOverflow-Thread gefunden:
+  // https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
+  if (access(*++argv, F_OK) == 0) {
 
-  int inputsize = read(inch, puffer, 1024);
-  write(1, puffer, inputsize);
+    int in = open(*argv, O_RDONLY);
+    int inputsize = read(in, &puffer, 1024);
+    write(1, puffer, inputsize);
+    close(in);
 
-  close(inch);
+  } else {
+    write(1, *argv, sizeof(*argv));
+    write(1, "\n", 1);
+  }
 
   return 0;
 }
