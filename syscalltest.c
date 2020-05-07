f660fde48d8a090d754f30eb1cc553276b9724b3
FrodoProductions <freddy@aerofloyd.de>
2020-05-07 18:08:11 +0200

    Aber jetzt

--- syscalltest.c
+++ syscalltest.c
@@ -1,20 +1,17 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 int main(int argc, char const *argv[]) {
 
   // Ein Puffer für den später eingelesenen Inhalt wird erzeugt (die Größe 100 ist jetzt nur arbiträr gesetzt).
   char puffer[100];
 
-
-  if (argc < 1) {
+  // Überprüft, ob Argumente gegeben wurden.
+  if (argc > 1) {
 
     // Nun wird für jeden Input iteriert, damit der Inhalt jeder Datei ausgegeben wird.
-    for (size_t i = 0; i < argc-1; i++) {
+    for (size_t i = 1; i < argc; i++) {
 
       // Es wird ein Kanal zur Datei mit Leserechten geöffnet.
       int in = open(argv[i], O_RDONLY);
@@ -29,16 +26,19 @@ int main(int argc, char const *argv[]) {
       close(in);
     }
 
+  // Dieser Fall wird aktiviert, wenn keine Argumente gegeben wurden, cat soll hier wie echo funktionieren.
   } else {
 
-    // Es wird ein Eingabeprompt geöffnet, der Inalt in den Puffer geschrieben und dessen Größe gespeichert.
-    int inputsize = read(0, &puffer, 1024);
+    // Diese unendliche Schleife ist hier, da cat, soweit ich das beurteilen kann, die echo-Funktion
+    // bis zum manuellen Abbruch unendlich oft ausführt.
+    while (1==1) {
 
-    // Jetzt wird der Puffer in die Konsole ausgegeben.
-    write(1, puffer, inputsize);
+      // Es wird ein Eingabeprompt geöffnet, der Inalt in den Puffer geschrieben und dessen Größe gespeichert.
+      int inputsize = read(0, &puffer, 1024);
 
-    // Dahinter kommt eine Newline, damit alles seine Ordnung hat :>
-    write(1, "\n", 1);
+      // Jetzt wird der Puffer in die Konsole ausgegeben.
+      write(1, puffer, inputsize);
+    }
   }
 
   return 0;
