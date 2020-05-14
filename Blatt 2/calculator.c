893dc612827c21685f79541463c53f8d3d23b255
kompetenzbestie <freddy@aerofloyd.de>
2020-05-14 17:33:47 +0200

    Dateien einlesen steht

--- Blatt 2/calculator.c
+++ Blatt 2/calculator.c
@@ -10,17 +10,40 @@ int calculate(int operand1, int operand2, char operator) {
 		case '*': operand1 = operand1 * operand2; break;
 		case '/': operand1 = operand1 / operand2; break;
 	}
-return operand1;
-}
 
+	return operand1;
+}
 
 int main(int argc, char const *argv[]) {
 
-	char puffer[100];
+	//char puffer[100];
 
 	if (argc > 1) {
 
-		//insert interesting stuff here
+		for (size_t i = 1; i < argc; i++) {
+			FILE* datei = fopen(argv[i], "r");
+
+			if (datei == NULL) {
+				printf("%s\n", "File not found.");
+				return 1;
+			}
+
+			char puffer[100];
+
+			int operand1, operand2;
+			char operator;
+
+			/* Funktioniert, ist mir aber nicht elegant genug
+			while (fscanf(datei, "%s\n", &puffer)==1) {
+				sscanf(puffer, "%d %c %d", &operand1, &operator, &operand2);
+				printf("%d\n", calculate(operand1, operand2, operator));
+			}
+			*/
+
+			while (fscanf(datei, "%d %c %d\n", &operand1, &operator, &operand2) == 3) {
+				printf("%d\n", calculate(operand1, operand2, operator));
+			}
+		}
 
 	} else {
 
