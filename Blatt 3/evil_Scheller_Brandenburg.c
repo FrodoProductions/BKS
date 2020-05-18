2ece3d2eb6ff15aa6fecc8ea75abd8c3b8439c35
kompetenzbestie <freddy@aerofloyd.de>
2020-05-18 20:27:11 +0200

    Jetzt auch mit Kommentaren

--- Blatt 3/evil_Scheller_Brandenburg.c
+++ Blatt 3/evil_Scheller_Brandenburg.c
@@ -4,17 +4,24 @@
 #include <unistd.h>
 #include <time.h>
 
+// https://www.linuxprogrammingblog.com/code-examples/sigaction wurde als Verständnishilfe
+// für die grundlegende Funktionalität von sigaction verwendet.
 
+// Die Handler-Funktion
 void endlos() {
 
-  struct timespec zeit;
+  // Ein timespec struct (das zeitgebende Argument für nanosleep) wird deklariert.
+  struct timespec printzeit;
 
-  zeit.tv_sec = 2;
+  // Die abzulaufende Zeit des timespecs wird auf 2 Sekunden gestellt.
+  printzeit.tv_sec = 2;
 
-  printf("%d\n", getpid());
+  // Die PID des Prozesses wird ausgegeben
+  printf("PID: %d\n", getpid());
 
   while (1) {
-    nanosleep(&zeit, NULL);
+    // Ein nanosleep mit einer Länge von 2 Sekunden wird durchgeführt.
+    nanosleep(&printzeit, NULL);
     printf("Endlosschleife\n");
   }
 
@@ -22,15 +29,25 @@ void endlos() {
 
 int main(int argc, char const *argv[]) {
 
+  // Ein timespec (das zeitgebende Argument für nanosleep) wird deklariert.
+  struct timespec zeit;
+
+  // Die abzulaufende Zeit des timespecs wird auf 1 Sekunde gestellt.
+  zeit.tv_sec = 1;
+
+  // Ein sigaction struct wird deklariert.
   struct sigaction action;
 
+  // endlos() wird als die Handler-Funktion der sigaction festgelegt.
   action.sa_handler = &endlos;
 
+  // Die sigaction wird aufgerufen. Sie soll hier auf SIGINT reagieren und ruft, wie in action deklariert,
+  // beim Auftreten dieses Signals die endlos() Funktion auf.
   sigaction(SIGINT, &action, NULL);
 
+  // Das Programm wird in eine Endlosschleife versetzt, damit es nicht sofort abbricht.
+  // Nicht besonders elegant, aber naja.
   while (1) {
-    sleep(10);
+    nanosleep(&zeit, NULL);
   }
-
-  return 0;
 }
