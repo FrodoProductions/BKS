c8b3bc51bc63a9aae1a5513ec7a4df435b66d54d
F03rD1 <suuchti.suuchti@gmail.com>
2020-05-22 04:51:57 +0200

    Fertig mama! Leider ein printf drin, aber write stinkt nach maggi

---
+++ Blatt 3/kill_Scheller_Brandenburg.c
@@ -0,0 +1,20 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[]) {
+
+  //ÜBerprüfen, ob der erste Input eine Signalnummer ist.
+  if (atoi(argv[1]) < 0){
+    //Für jede eingegebene PID wird der kill Befehl aufgerufen
+    for (int i = 2; i < argc; i++){
+      kill(atoi(argv[i]), atoi(argv[1]));
+    }
+  }
+  //Analog wie oben, jedoch mit festen SIGTERM
+  else {
+    for (int i = 1; i < argc; i++){
+      kill(atoi(argv[i]), SIGTERM);
+  }
+}
